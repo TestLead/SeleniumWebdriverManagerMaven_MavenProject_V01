fa9c8c0a962ac34fa9e0148a41f3bbee0d09bcea
PARIJAT <rps.rjn@gmail.com>
2020-01-30 16:17:00 +0530

    Third time commited

--- src/test/java/tests/TestNGExample.java
+++ src/test/java/tests/TestNGExample.java
@@ -8,15 +8,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 //Writing TestNG example
 //To automate Flip cart
+
 public class TestNGExample {
 	
 	@Test
-	public void test()
+	public void test() throws InterruptedException
 	{
 		Reporter.log("Start Flip cart home page" , true);
 		WebDriverManager.firefoxdriver().setup();         //System.setProperty("webdriver.gecko.driver", "../MavenProject/exe/geckodriver.exe");
 	    WebDriver driver = new FirefoxDriver();       
 	    //driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 	    driver.navigate().to("https://www.flipkart.com/");
+	    Thread.sleep(2000);
+	    driver.close();
 	}
 }
