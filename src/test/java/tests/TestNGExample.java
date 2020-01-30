d76a408a445fcbf8ef18283c49492b73ad45c225
PARIJAT <rps.rjn@gmail.com>
2020-01-30 16:03:18 +0530

    TestNGExample file commited

--- src/test/java/tests/TestNGExample.java
+++ src/test/java/tests/TestNGExample.java
@@ -1,2 +1,22 @@
+package tests;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 //Writing TestNG example
-//To automate Flipcart
+//To automate Flip cart
+public class TestNGExample {
+	
+	@Test
+	public void test()
+	{
+		Reporter.log("Start Flip cart home page" , true);
+		WebDriverManager.firefoxdriver().setup();         //System.setProperty("webdriver.gecko.driver", "../MavenProject/exe/geckodriver.exe");
+	    WebDriver driver = new FirefoxDriver();       
+	    //driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	    driver.navigate().to("https://www.flipkart.com/");
+	}
+}
