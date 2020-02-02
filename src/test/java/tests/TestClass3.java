9a5dbbc0b96b06d8176011fdc49ebf5d73c70484
PARIJAT <rps.rjn@gmail.com>
2020-02-02 14:08:52 +0530

    Fourth test class added

---
+++ src/test/java/tests/TestClass3.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TestClass3 {
+	
+	@Test
+	public void test3(){
+		
+		System.out.println("=======================Testing Maven project =================");
+	}
+
+}
