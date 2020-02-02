17337c3adb14e1335d9fc6411424faf3471c2b23
PARIJAT <rps.rjn@gmail.com>
2020-02-02 15:35:16 +0530

    Deleted all files within allure-results folder so that get the fresh result after Jenkin execution and added 5th TC

---
+++ src/test/java/tests/TestClass4.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TestClass4 {
+
+	@Test
+	public void test4(){
+		
+		System.out.println("==========Testing Maven GIT&GITHUB project with test4() ============");
+	}
+}
